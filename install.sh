5a102541770a1cc07e31f61cf9c0cab72ccdf004
Rico Soleng <ricosoleng@protonmail.com>
2023-01-27 22:40:38 +0100

    Removed automatic start of OBS on install.

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ sudo apt-get install -y xclip xsel
 sudo apt install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.obsproject.Studio
-flatpak run com.obsproject.Studio
 
 echo ""
 
